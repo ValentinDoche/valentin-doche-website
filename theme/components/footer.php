e228b78a4b8917b60a5a2cbe53ac8ad428b806d7
Valentin Doche <contact@valentindoche.com>
2021-01-15 14:24:48 +0100

    Bug Fix url footer.php logo

--- theme/components/footer.php
+++ theme/components/footer.php
@@ -6,7 +6,7 @@ function footer(){ ?>
                 <div class="row copy-right">
                     <div class="col-md-6 col-md-offset-3 text-center">
                         <!-- Footer logo -->
-                        <h2><a class="footer-logo" href="index.html"><img src="/theme/assets/images/logo.webp" height="100"></a></h2>
+                        <h2><a class="footer-logo" href="/"><img src="/theme/assets/images/logo.webp" height="100"></a></h2>
                         <!-- Social -->
                         <p class="social-icon">
                             <a href="https://www.facebook.com/dochevalentin/"><i class="fab fa-facebook-square"></i></a>
